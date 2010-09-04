8f654ca5ceda7b871273b0ff9359c75adcee30b9
fedemartino <fedemartino@89bacaef-1c79-b071-9ecf-0a6c37b9ac43>
2010-09-04 19:28:46 +0000

    Agrego Parser XML

---
+++ trunk/SnmpMonitor/XMLData/XMLData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Base
+{
+    class XMLData
+    {
+        XmlDocument doc;
+        XmlReader reader;
+        public XMLData()
+        {
+            doc = new XmlDocument();
+            string path = Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase), "conexion.xml");
+            reader = XmlReader.Create(path);
+            doc.Load(reader);
+        }
+        public string GetParametro(string parametro)
+        {
+            String valor = "";
+            XmlNode nodo = doc.SelectSingleNode("/Data/Parametros");
+            valor = nodo[parametro].Attributes["valor"].Value;
+            valor.Trim();
+            return valor;
+        }
+    }
+}
